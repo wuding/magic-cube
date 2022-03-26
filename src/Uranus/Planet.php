3b92481f030048da5dc9505bdc51d8877c625233
wuding <netjoin@gmail.com>
2022-03-26 20:46:39 +0800

    新增 Uranus 目录

---
+++ src/Uranus/Planet.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * URL
+ * Request & Response
+ * Anchor | hyperlink
+ * Network & Upload & Save
+ */
+
+namespace MagicCube\Uranus;
+
+class Planet
+{
+    const VERSION = '22.3.26';
+}
